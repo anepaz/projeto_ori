703a0684bfe49ff502ad0f0e0f86cd0a72980cc6
anepaz <anelisespaz@gmail.com>
2021-10-18 21:22:48 -0300

    js animation

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if(window.SimpleSlide) {
+
+new SimpleSlide({
+    slide: "quote", //nome do slide
+    time: 5000 //tempo de transição = 5s
+});
+
+new SimpleSlide({
+    slide: "portfolio", 
+    time: 5000,
+    nav: true
+});
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}

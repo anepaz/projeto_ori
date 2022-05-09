738b415f270c89dafa277156be0ca652563b49a7
anepaz <anelisespaz@gmail.com>
2022-05-09 16:16:49 -0300

    fix indentation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 if(window.SimpleSlide) {
 
-new SimpleSlide({
+    new SimpleSlide({
     slide: "quote", //nome do slide
     time: 5000 //tempo de transição = 5s
 });
 
-new SimpleSlide({
-    slide: "portfolio", 
-    time: 5000,
-    nav: true
-});
+    new SimpleSlide({
+        slide: "portfolio", 
+        time: 5000,
+        nav: true
+    });
 }
 
 if(window.SimpleAnime) {
